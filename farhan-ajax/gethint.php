07bf0f67a4a887f50b84e84d760c1f868b186c56
M. Farhan Adhitya <38453230+farhanadhitya@users.noreply.github.com>
2018-04-24 14:45:56 +0700

    Add files via upload

---
+++ farhan-ajax/gethint.php
@@ -0,0 +1,28 @@
+<?php
+// array with names
+$a[] = "Farhan" ;
+$a[] = "Avi";
+$a[] = "Kurnia";
+$a[] = "Antonio";
+$a[] = "Alif";
+
+//get the q parameter from URL
+$q = $_REQUEST["q"];
+$hint = "";
+// lookup all hints from array $q is different from ""
+if($q !== ""){
+	$q= strtolower($q);
+	$len=strlen($q);
+	foreach($a as $name){
+	if(stristr($q, substr($name, 0, $len))){
+	if($hint === ""){
+	$hint = $name;
+	} else{
+	$hint .= ", $name";
+	}
+	}
+	}
+	}
+// output " no suggestion" if no hint was found or output correct values
+echo $hint === ""? "Gak ada boss!": $hint;
+?>
